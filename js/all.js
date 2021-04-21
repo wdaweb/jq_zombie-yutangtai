ab7a9c4d61a7c6d856784d21a640656e980776e3
yutangtai <dajiu0224@gmail.com>
2021-04-21 15:12:28 +0800

    思緒有點亂，胡搞瞎搞一翻
    1. 改成增加 div 而非 img : 想以圖片填滿背景的方式來做 
    2. 已知確定的變數有：
        (1) 產生的水果數量 
        (2) 水果種類 
        (3) 產生水果的 x 座標
        (4) 落下的速度
    3. 動畫不知道要怎麼加 qq

--- js/all.js
+++ js/all.js
@@ -18,23 +18,29 @@
 1.Sweetalert 顯示分數(和最高分數)，按下確定後回到開始畫面
 */
 
+const melon = `background-image:url("./images/watermelon_all.png")`
+const coconut = `background-image:url("/images/coconut_all.png")`
+const apple = `background-image:url("/images/apple_all2.png")`
+const banana = `background-image:url("/images/banana_all.png")`
+const orange = `background-image:url("/images/orange_all.png")`
+const lemon = `background-image:url("/images/lemon_all.png")`
+
+const fruits = [melon, coconut, apple, banana, orange, lemon]
+
 //掉水果(X 軸隨機，Y 軸改變)
 // 水果掉落的範圍
 let regionWidth = $('.fruitRegion').outerWidth()
 let regionHeight = $('.fruitRegion').outerHeight()
-let startX = $('.fruitRegion').offset().left
-let startY = $('.fruitRegion').offset().top
-let endX = startX + regionWidth
-let endY = $('#footer').offset().top
-
-
-
+let ltX = $('.fruitRegion').offset().left
+let ltY = $('.fruitRegion').offset().top
+let rtX = ltX + regionWidth
+let lbY = $('#footer').offset().top
 
 console.log('寬:' + regionWidth + ',' + '高:' + regionHeight)
-console.log('左上:' + startX + ',' + startY)
-console.log('右上:' + endX + ',' + startY)
-console.log('右下:' + endX + ',' + endY)
-console.log('左下:' + startX + ',' + endY)
+console.log('左上:' + ltX + ',' + ltY)
+console.log('右上:' + rtX + ',' + ltY)
+console.log('右下:' + rtX + ',' + lbY)
+console.log('左下:' + ltX + ',' + lbY)
 
 // 按下 START 會進入遊戲畫面
 $('#startBtn').click(function () {
@@ -47,6 +53,7 @@ $('#startBtn').click(function () {
     let timer = setInterval(function () {
       countdown--
       randomData()
+      addFruit()
       fruitFall()
       $('.text-countdown').text(countdown)
       if (countdown === 0) {
@@ -60,53 +67,62 @@ $('#startBtn').click(function () {
 // 滑鼠在該區域裡的相對位置
 $('.fruitRegion').mousemove(function (e) {
   var elm = $(this)
-  var xPos = e.pageX - elm.offset().left
-  var yPos = e.pageY - elm.offset().top
-  console.log('滑鼠位置:' + xPos + ',' + yPos)
+  var mouseX = e.pageX - elm.offset().left
+  var mouseY = e.pageY - elm.offset().top
+  console.log('滑鼠位置:' + mouseX + ',' + mouseY)
 })
 
 //隨機數字
-function randomNum(min, max){
-  return Math.floor(Math.random() * (max - min)) + min
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min))
 }
-// 隨機水果
-const melon = `<img src="./images/watermelon_all.png" alt="西瓜" style="height: 105px; width: auto;">`
-const coconut = `<img src="./images/coconut_all.png" alt="椰子" style="height: 75px; width: auto;">`
-const apple = `<img src="./images/apple_all2.png" alt="蘋果" style="height: 80px; width: auto;">`
-const banana = `<img src="./images/banana_all.png" alt="香蕉" style="height: 100px; width: auto;">`
-const orange = `<img src="./images/orange_all.png" alt="柳丁" style="height: 60px; width: auto;">`
-const lemon = `<img src="./images/lemon_all.png" alt="檸檬" style="height: 50px; width: auto;">`
-const fruits = [melon, coconut, apple, banana, orange, lemon]
-let fruitIndex  //隨機水果索引值
+// 隨機數據
+let fruitIndex //隨機水果索引值
 let fruit //隨機水果
-let fruitNum //水果數量
-let velocity  //隨機速度
-let posX //隨機 x 座標
-function randomData(){  
-  fruitNum = randomNum(2, 5)
-  for(let i=1; i<=fruitNum; i++){
-    fruitIndex = randomNum(0, 6)
-    fruit = fruits[fruitIndex]
-    velocity = randomNum(850, 10000)
-    posX  = randomNum(startX, endX)
-    postY = randomNum(startY, endY)
-    
-    $('.fruitRegion').append(`<span class="fruit">${fruits[fruitIndex]}</span>`)
-  }
+let fruitNum = 0 //水果數量
+let velocity = 0 //隨機速度
+let posX = 0 //隨機 x 座標
+
+function randomData() {
+  fruitNum = randomNum(1, 5)
+  fruitIndex = randomNum(0, 6)
+  velocity = randomNum(850, 10000)
+  posX = randomNum(ltX, rtX)
+  postY = randomNum(lbY, ltY)
 }
 
+//增加水果
+function addFruit() {
+  randomData()
+  fruit = fruits[fruitIndex]
+  $('.fruitRegion').append(`<div class="fruitBox"></div>`)
+  $('.fruitBox').animate({ left: '+=100px' })
+  $('.fruit').addClass('move')
+}
 
 //水果掉下來
-function fruitFall(){
-  console.log(startX + posX + 'px')
-  console.log(startY + postY + 'px')
-  if($('.fruitRegion span').length > 0){
-    $('.fruit').animate({
-      // width: '+=100px',
-      translateX: `+=${startX + posX}`,
-      translateY: `+=${startY + postY}`
-    },1000)
-  }else{
-    $('.fruit').css('display','none')
+function fruitFall() {
+  // console.log(posX + 'px')
+  // console.log(postY + 'px')
+  if ($('.fruitRegion span').length > 0) {
+    posX = randomNum(ltX, rtX)
+    console.log('x 座標:' + posX)
+    console.log('span 數量:' + $('.fruitRegion span').length)
+    for (let i = 0; i < $('.fruitRegion span').length; i++) {
+      console.log('i 迴圈:' + i)
+      $('.fruit')
+        .eq(i)
+        .css({ left: `${posX}px` })
+    }
+    // $('.fruit').animate(
+    //   {
+    //     // width: '+=100px',
+    //     left: `${startX + posX}px`,
+    //     top: `${startY + postY}px`
+    //   },
+    //   1000
+    // )
+  } else {
+    $('.fruit').css('display', 'none')
   }
 }
