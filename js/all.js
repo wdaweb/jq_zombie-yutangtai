4bba8634d4d5f789a55bd8dc4b4436a19b0dcdf0
yutangtai <dajiu0224@gmail.com>
2021-04-26 16:37:02 +0800

    解決 mouseover 多次移入會重複計分的問題 + 遊戲結束後清空 fruitRegion
    1. if else 改成 switch
    2. 老師修改

--- js/all.js
+++ js/all.js
@@ -8,8 +8,8 @@
 二、開始遊戲
 1.在畫面隨機掉下水果(由上往下)
 2.滑鼠滑到水果會把水果切一半並得分
-3.水果種類和加分：西瓜(+1)、椰子(+1)、蘋果(+2)、橘子(+2)、檸檬(+2)、香蕉(+3)
-4.有時間限制：60 秒
+3.水果種類和加分：西瓜(+5)、椰子(+4)、香蕉(+3)、蘋果(+2)、橘子(+1)、檸檬(+1)
+4.有時間限制：30 秒
 5.按下【暫停鍵】時間會暫停、水果暫停落下，且【暫停鍵】換成【開始鍵】
 6.按下【開始鍵】後遊戲繼續
 (7.記錄最高分)
@@ -35,61 +35,55 @@ let ltX = $('.fruitRegion').offset().left
 let ltY = $('.fruitRegion').offset().top
 let rtX = ltX + regionWidth
 let lbY = $('#footer').offset().top
-let countdown = 10
+let countdown = 30
 
-console.log('寬:' + regionWidth + ',' + '高:' + regionHeight)
-console.log('左上:' + ltX + ',' + ltY)
-console.log('右上:' + rtX + ',' + ltY)
-console.log('右下:' + rtX + ',' + lbY)
-console.log('左下:' + ltX + ',' + lbY)
+// console.log('寬:' + regionWidth + ',' + '高:' + regionHeight)
+// console.log('左上:' + ltX + ',' + ltY)
+// console.log('右上:' + rtX + ',' + ltY)
+// console.log('右下:' + rtX + ',' + lbY)
+// console.log('左下:' + ltX + ',' + lbY)
 
 // 按下 START 會進入遊戲畫面
 $('#startBtn').click(function () {
   $('#startPage').css('display', 'none')
-  $('.fruitRegion').empty()  //為何沒效果?
+  $('.fruitRegion').empty()
   $('.text-score').text('0')
 
   let timer = setInterval(function () {
     countdown--
     randomData()
-    addFruit()
-    
+
     $('.text-countdown').text(countdown)
     if (countdown === 0) {
       clearInterval(timer)
+      clearInterval(fruitFallFrequency)
       $('#startPage').css('display', '')
       Swal.fire({
-        icon:'info',
-        title:'Game Over!!!',
-        text:`Your score is ${score}`
+        icon: 'info',
+        title: 'Game Over!!!',
+        text: `Your score is ${score}`
       })
-      countdown = 10
+      countdown = 30
       score = 0
+      $('.fruitRegion').empty()
     }
   }, 1000)
-})
 
-// 滑鼠在該區域裡的相對位置
-// $('.fruitRegion').mousemove(function (e) {
-//   var elm = $(this)
-//   var mouseX = e.pageX - elm.offset().left
-//   var mouseY = e.pageY - elm.offset().top
-//   console.log('滑鼠位置:' + mouseX + ',' + mouseY)
-// })
+  fruitFallFrequency = setInterval(addFruit, 500)
+})
 
 //隨機數字
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min))
 }
+
 // 隨機數據
 let fruitIndex //隨機水果索引值
 let fruit //隨機水果
-let fruitNum //水果數量
 let velocity //隨機速度
 let posX //隨機 x 座標
 
 function randomData() {
-  fruitNum = randomNum(1, 2)
   fruitIndex = randomNum(0, 6)
   posX = randomNum(ltX + 105, rtX) //加上最大顆的水果寬度，不然會超出邊界
   posY = randomNum(lbY, ltY)
@@ -98,21 +92,17 @@ function randomData() {
 
 //增加水果
 let index = 0
+let fruitFallFrequency = 0
 function addFruit() {
-  let fruitFallFrequency = setInterval(function () {
-    randomData()
-    index++
-    fruit = fruits[fruitIndex]
-    $('.fruitRegion').append(`<span class="fruit" id="${index}">${fruits[fruitIndex]}</span>`)
+  randomData()
+  index++
+  fruit = fruits[fruitIndex]
+  $('.fruitRegion').append(`<span class="fruit" id="${index}">${fruits[fruitIndex]}</span>`)
 
-    $('.fruitRegion span')
-      .eq($('.fruitRegion span').length - 1)
-      .css({ left: `${posX}px` })
-    fruitFall(fruitIndex)
-    if (countdown === 0) {
-      clearInterval(fruitFallFrequency)
-    }
-  }, 5000)
+  $('.fruitRegion span')
+    .eq($('.fruitRegion span').length - 1)
+    .css({ left: `${posX}px` })
+  fruitFall(fruitIndex)
 }
 
 //水果掉下來
@@ -125,47 +115,43 @@ function fruitFall(fruitIndex) {
 // 滑鼠碰到水果就變切片
 let score = 0
 $('.fruitRegion').on('mouseover', '.fruit', function () {
-  if ($(this).find('img').attr('id') === 'melon') {
-    score += 5; 
-    $(this).find('img').attr('src', '/images/watermelon_slice.png')
+  if ($(this).hasClass('cut')) return
 
-    setTimeout(function () {
-      $(this).hide()
-    }, 10)
-  } else if ($(this).find('img').attr('id') === 'coconut') {
-    score += 4
-    $(this).find('img').attr('src', '/images/coconut_half.png')
-    setTimeout(function () {      
-      $(this).hide()
-    }, 10)
-  } else if ($(this).find('img').attr('id') === 'banana') {
-    score += 3
-    $(this).find('img').attr('src', '/images/banana_slice.png')
-    setTimeout(function () {
-      $(this).hide()
-    }, 10)
-  } else if ($(this).find('img').attr('id') === 'apple') {
-    score += 2
-    $(this).find('img').attr('src', '/images/apple_slice.png')
-    setTimeout(function () {
-      $(this).hide()
-    }, 10)
-  } else if ($(this).find('img').attr('id') === 'orange') {
-    score += 1
-    $(this).find('img').attr('src', '/images/orange_4.png')
-    setTimeout(function () {
-      $(this).hide()
-    }, 10)
-  } else if ($(this).find('img').attr('id') === 'lemon') {
-    score += 1
-    $(this).find('img').attr('src', '/images/lemon_half2.png')
-    // $(this).onmouseover = null;
-    
-    setTimeout(function () {
-      $(this).hide()
-    }, 10)
+  switch ($(this).find('img').attr('id')) {
+    case 'melon':
+      score += 5
+      $(this).find('img').attr('src', '/images/watermelon_slice.png')
+      $(this).addClass('cut')
+      break
+    case 'coconut':
+      score += 4
+      $(this).find('img').attr('src', '/images/coconut_half.png')
+      $(this).addClass('cut')
+      break
+    case 'banana':
+      score += 3
+      $(this).find('img').attr('src', '/images/banana_slice.png')
+      $(this).addClass('cut')
+      break
+    case 'apple':
+      score += 2
+      $(this).find('img').attr('src', '/images/apple_slice.png')
+      $(this).addClass('cut')
+      break
+    case 'orange':
+      score += 1
+      $(this).find('img').attr('src', '/images/orange_4.png')
+      $(this).addClass('cut')
+      break
+    case 'lemon':
+      score += 1
+      $(this).find('img').attr('src', '/images/lemon_half2.png')
+      $(this).addClass('cut')
+      break
+    default:
+      console.log('沒抓到')
+      break
   }
-  console.log(score)
   $('.text-score').text(score)
 })
 
